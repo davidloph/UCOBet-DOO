f79d06c86142dd806c2274008cd73464bae0f937
davidlop <davidloperahenao@gmail.com>
2024-08-04 12:39:56 -0500

    Se puede  escoger la cantidad de números a apostas (numerosAApostar).

--- src/DatosJugador.java
+++ src/DatosJugador.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DatosJugador {
     public static void datosJugador(ArrayList<Jugador> jugadores) {
@@ -7,8 +8,10 @@ public class DatosJugador {
         String email;
         String cedula;
         String celular;
+        int numerosAApostar;
         String apuesta;
         String numSorteo;
+
         while(true) {
             nombre = JOptionPane.showInputDialog("Nombre: ");
             if (nombre == null || nombre.isBlank()) {
@@ -42,6 +45,14 @@ public class DatosJugador {
             }
         }
         while (true) {
+            numerosAApostar = JOptionPane.showOptionDialog(null, "Escoge la cantidad de números a apostar:", "UCO Bet", 0, 0,null, Arrays.asList("1", "2", "3", "4").toArray(), null) + 1;
+            if (numerosAApostar == 0) {
+                JOptionPane.showMessageDialog(null, "Ingrese un valor.");
+            } else {
+                break;
+            }
+        }
+        while (true) {
             apuesta = JOptionPane.showInputDialog("Valor apuesta: ");
             if (apuesta == null || apuesta.isBlank() || !apuesta.matches("\\d+") || apuesta.contains("-")) {
                 JOptionPane.showMessageDialog(null, "Ingrese un valor válido.");
@@ -51,7 +62,7 @@ public class DatosJugador {
         }
         while (true) {
             numSorteo = JOptionPane.showInputDialog("Sorteo: ");
-            if (numSorteo == null || numSorteo.isBlank() || numSorteo.length() > 4 || !numSorteo.matches("\\d+")) {
+            if (numSorteo == null || numSorteo.isBlank() || numSorteo.length() != numerosAApostar || !numSorteo.matches("\\d+")) {
                 JOptionPane.showMessageDialog(null, "Ingrese un numero válido.");
             } else {
                 break;
@@ -60,7 +71,7 @@ public class DatosJugador {
 
 
 
-        Jugador jugador = new Jugador(nombre, email, cedula, celular, apuesta, numSorteo);
+        Jugador jugador = new Jugador(nombre, email, cedula, celular, numerosAApostar, apuesta, numSorteo);
         JOptionPane.showMessageDialog(null, "Jugador guardado:\n\n" + jugador.toString());
         jugadores.add(jugador);
 
