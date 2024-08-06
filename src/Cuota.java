cf2545115dd9e5ea97433bf60945c4f4ea8850a3
davidlop <davidloperahenao@gmail.com>
2024-08-06 15:01:24 -0500

    Cuotas como constantes.

--- src/Cuota.java
+++ src/Cuota.java
@@ -1,38 +1,6 @@
 public class Cuota {
-    private static int CUOTA1NUMERO = 10;
-    private int CUOTA2NUMEROS = 15;
-    private int CUOTA3NUMEROS = 50;
-    private int CUOTA4NUMEROS = 10000;
-
-    public int getCuota1() {
-        return CUOTA1NUMERO;
-    }
-
-    public void setCuota1(int CUOTA1NUMERO) {
-        this.CUOTA1NUMERO = CUOTA1NUMERO;
-    }
-
-    public int getCUOTA2NUMEROS() {
-        return CUOTA2NUMEROS;
-    }
-
-    public void setCUOTA2NUMEROS(int CUOTA2NUMEROS) {
-        this.CUOTA2NUMEROS = CUOTA2NUMEROS;
-    }
-
-    public int getCuota3() {
-        return CUOTA3NUMEROS;
-    }
-
-    public void setCuota3(int CUOTA3NUMEROS) {
-        this.CUOTA3NUMEROS = CUOTA3NUMEROS;
-    }
-
-    public int getCUOTA4NUMEROS() {
-        return CUOTA4NUMEROS;
-    }
-
-    public void setCUOTA4NUMEROS(int CUOTA4NUMEROS) {
-        this.CUOTA4NUMEROS = CUOTA4NUMEROS;
-    }
+    public static int CUOTA1NUMERO = 10;
+    public static int CUOTA2NUMEROS = 15;
+    public static int CUOTA3NUMEROS = 50;
+    public static int CUOTA4NUMEROS = 10000;
 }
